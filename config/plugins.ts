b73c4149d46c46f92ab752ba764166fdd53c3dcb
Sebastian Lancheros <sebastian.lancheros@proper.ai>
2024-11-28 11:53:26 -0500

    let strapi ad cloudinary display pdf files

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,9 +6,12 @@ export default ({ env }) => ({
         cloud_name: env("CLOUDINARY_NAME"),
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
+        resource_type: "raw",
       },
       actionOptions: {
-        upload: {},
+        upload: {
+          folder: "uploads",
+        },
         delete: {},
       },
     },
